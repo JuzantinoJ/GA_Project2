cfbd91f9bebfee9178211ad4e4ef7e6ec27b2b1b
JuzantinoJ <juzantinoj@gmail.com>
2023-08-07 02:04:35 +0800

    removed some files

--- social-media-app/src/client.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-const supabaseUrl = "https://bkrvcsfzycqmoqwgaxeh.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJrcnZjc2Z6eWNxbW9xd2dheGVoIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODk2OTg0NjAsImV4cCI6MjAwNTI3NDQ2MH0.t5O3PqAo39oDwY2Eibyygk0q7rgQ6Lyr2Xt5QaoGJaI";
-export const supabase = createClient(supabaseUrl, supabaseKey);
